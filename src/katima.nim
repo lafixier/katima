d88c58cf3680feaa75fb72c1a4083f9e56bcb33d
lafixier <lfxr@proton.me>
2023-02-04 17:37:57 +0900

    feat: convertコマンドを実装

--- src/katima.nim
+++ src/katima.nim
@@ -11,10 +11,17 @@ proc repl(args: seq[string]) =
   ## start the REPL mode
   runRepl()
 
+proc convert(
+  oldCharFormsSrc = "", newCharFormsSrc = "",
+    reversalSrc = "", args: seq[string]) =
+  if oldCharFormsSrc != "": echo oldCharFormsSrc.toOldForm
+  if newCharFormsSrc != "": echo newCharFormsSrc.toNewForm
+  if reversalSrc != "": echo reversalSrc.toReversal
+
 
 when isMainModule:
   import cligen
-  dispatchMulti([repl])
+  dispatchMulti([repl], [convert])
   # echo "医学".toOldForm
   # echo "醫學".toNewForm
   # echo "医學".toReversal
